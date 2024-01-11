1bb865b7da1d6eaf18cf7b9f485674664ff67813
Karandeep Singh Pabla <ks1895304@gmail.com>
2024-01-12 00:59:14 +0530

    v1.0.1

--- src/main/java/com/example/browser2/HelloController.java
+++ src/main/java/com/example/browser2/HelloController.java
@@ -10,6 +10,7 @@ import javafx.scene.web.WebEngine;
 import javafx.scene.web.WebHistory;
 import javafx.scene.web.WebView;
 import java.net.URL;
+import java.util.List;
 import java.util.ResourceBundle;
 
 public class HelloController implements Initializable {
@@ -27,7 +28,7 @@ public class HelloController implements Initializable {
     private WebEngine engine;
 
     @FXML
-    private WebHistory history,newTabHistory;
+    private WebHistory history;
 
     @FXML
     private Tab initialTab;
@@ -39,32 +40,6 @@ public class HelloController implements Initializable {
     @FXML
     private ProgressBar progress;
 
-    @FXML
-    private Tab tab2;
-
-    @FXML
-    private AnchorPane pane;
-
-    @FXML
-    private Button backbtn;
-
-    @FXML
-    private Button forwadbtn;
-
-    @FXML
-    private Button reloadbtn;
-
-    @FXML
-    private TextField searchBar;
-
-    @FXML
-    private Button optionsbtn;
-
-    @FXML
-    private WebView page;
-
-    private WebEngine newTabEngine;
-
     public void checkLastTab(){
         if(tabPane.getTabs().size()==2){
             Platform.exit();
@@ -73,48 +48,32 @@ public class HelloController implements Initializable {
 
     //Code For Every New tab Added
     public void addNewTab(){
-        tab2 = new Tab("");
-        pane = new AnchorPane();
-        backbtn = new Button("Back");
+        Tab tab2 = new Tab();
+        AnchorPane pane = new AnchorPane();
+        Button backbtn = new Button("Back");
         AnchorPane.setLeftAnchor(backbtn,6.0);
         pane.getChildren().add(backbtn);
-        backbtn.setOnAction(e->{
-            backPageForNewtab();
-        });
 
-        forwadbtn = new Button("Forwad");
+        Button forwadbtn = new Button("Forwad");
         AnchorPane.setLeftAnchor(forwadbtn,55.0);
         pane.getChildren().add(forwadbtn);
-        forwadbtn.setOnAction(e->{
-            forwadPageForNewtab();
-        });
 
-        reloadbtn = new Button("Reload");
+        Button reloadbtn = new Button("Reload");
         AnchorPane.setLeftAnchor(reloadbtn,117.0);
         pane.getChildren().add(reloadbtn);
-        forwadbtn.setOnAction(e->{
-            reloadPageForNewtab();
-        });
 
-        searchBar = new TextField();
+        TextField searchBar = new TextField();
         searchBar.setPrefWidth(337);
         searchBar.setPrefHeight(26);
         AnchorPane.setLeftAnchor(searchBar,182.0);
         AnchorPane.setRightAnchor(searchBar,81.0);
         pane.getChildren().add(searchBar);
-        searchBar.setOnAction(e->{
-            loadNewTabPage();
-        });
 
-        optionsbtn = new Button("Options");
+        Button optionsbtn = new Button("Options");
         AnchorPane.setRightAnchor(optionsbtn,14.0);
         pane.getChildren().add(optionsbtn);
-        //not defined yet
-        optionsbtn.setOnAction(e->{
-            optionsbtnForNewtab();
-        });
 
-        page = new WebView();
+        WebView page = new WebView();
         page.setPrefWidth(600);
         page.setPrefHeight(344);
         AnchorPane.setTopAnchor(page,29.0);
@@ -132,62 +91,57 @@ public class HelloController implements Initializable {
                 Platform.exit();
             }
         });
-        initializeNewTab();
-    }
 
-    private void initializeNewTab() {
-//        newTabEngine = new WebEngine();
-        newTabEngine = page.getEngine();
-        String newTabHomePage = "www.google.com";
-        String newTabTitle = "Google";
-        searchBar.setText(newTabHomePage);
-        tab2.setText(newTabTitle);
-        loadNewTabPage();
-    }
+        WebEngine newTabEngine = page.getEngine();
+        newTabEngine.load("https://www.google.com");
+        System.out.println("Initial title "+newTabEngine.getTitle());
+        WebHistory webHistory = newTabEngine.getHistory();
+        System.out.println("Current url = "+newTabEngine.getLocation());
 
-    private void loadNewTabPage() {
-        if(searchBar.getText().startsWith("https")){
-            newTabEngine.load(searchBar.getText());
-        }
-        else if (searchBar.getText().startsWith("www")){
-            newTabEngine.load("https://"+searchBar.getText());
-        }
-        else{
-            newTabEngine.load("https://www.google.com/search?q="+searchBar.getText());
-        }
-//        progress.progressProperty().bind(newTabEngine.getLoadWorker().progressProperty());
-    }
+        searchBar.setOnAction(e->{
+            if(searchBar.getText().startsWith("https")){
+                newTabEngine.load(searchBar.getText());
+            }
+            else if (searchBar.getText().startsWith("www")){
+                newTabEngine.load("https://"+searchBar.getText());
+            }
+            else{
+                newTabEngine.load("https://www.google.com/search?q="+searchBar.getText());
+            }
+        });
 
-    private void optionsbtnForNewtab() {
+        newTabEngine.locationProperty().addListener((observable,oldValue,newValue)->{
+            searchBar.setText(newTabEngine.getLocation());
+        });
 
-    }
+        newTabEngine.titleProperty().addListener((observable,oldValue,newValue)->{
+            tab2.setText(newTabEngine.getTitle());
+            System.out.println("Current title "+newTabEngine.getTitle());
+        });
 
-    private void reloadPageForNewtab() {
-        newTabEngine.reload();
-    }
+        backbtn.setOnAction(e->{
+            ObservableList<WebHistory.Entry> entriees = webHistory.getEntries();
+            webHistory.go(-1);
+            if(webHistory.getCurrentIndex()==0){
+                tab2.setText("Google");
+                searchBar.setText("https://www.google.com");
+            }
+            else {
+                tab2.setText(entriees.get(webHistory.getCurrentIndex()).getTitle());
+                searchBar.setText(entriees.get(webHistory.getCurrentIndex()).getUrl());
+            }
+        });
 
-    private void forwadPageForNewtab() {
-        newTabHistory = page.getEngine().getHistory();
-        ObservableList<WebHistory.Entry> entries = newTabHistory.getEntries();
-        newTabHistory.go(1);
-        System.out.println("Entries for new tab is : "+entries);
-        tab2.setText(entries.get(newTabHistory.getCurrentIndex()).getTitle());
-        searchBar.setText(entries.get(newTabHistory.getCurrentIndex()).getUrl());
-    }
+        forwadbtn.setOnAction(e->{
+            ObservableList<WebHistory.Entry> entriees = webHistory.getEntries();
+            webHistory.go(1);
+            tab2.setText(entriees.get(webHistory.getCurrentIndex()).getTitle());
+            searchBar.setText(entriees.get(webHistory.getCurrentIndex()).getUrl());
+        });
 
-    private void backPageForNewtab() {
-        newTabHistory = page.getEngine().getHistory();
-        ObservableList<WebHistory.Entry> entries = newTabHistory.getEntries();
-        System.out.println("Entries for new tab is : "+entries);
-        newTabHistory.go(-1);
-        if(newTabHistory.getCurrentIndex()==0){
-            tab2.setText("Google");
-            searchBar.setText("https://www.google.com");
-        }
-        else {
-            tab2.setText(entries.get(newTabHistory.getCurrentIndex()).getTitle());
-            searchBar.setText(entries.get(newTabHistory.getCurrentIndex()).getUrl());
-        }
+        reloadbtn.setOnAction(e->{
+            newTabEngine.reload();
+        });
     }
 
     //Code For The initial tab
@@ -195,9 +149,7 @@ public class HelloController implements Initializable {
     public void initialize(URL url, ResourceBundle resourceBundle) {
         engine = webView.getEngine();
         homePage = "www.google.com";
-        initialTabName = "Google";
         textField.setText(homePage);
-        initialTab.setText(initialTabName);
         loadPage();
     }
 
@@ -212,6 +164,12 @@ public class HelloController implements Initializable {
             engine.load("https://www.google.com/search?q="+textField.getText());
         }
         progress.progressProperty().bind(engine.getLoadWorker().progressProperty());
+        engine.locationProperty().addListener((observable,oldValue,newValue)->{
+            textField.setText(engine.getLocation());
+        });
+        engine.titleProperty().addListener((observable,oldValue,newValue)->{
+            initialTab.setText(engine.getTitle());
+        });
     }
 
     public void reloadPage(){
